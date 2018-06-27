5f0f1a551b9f1907f0dd511bfeb57c76deaa5bd9
まゆり吉積 <mayuriyoshizumi@mayuriyoshidzuminomacbook-ea.local>
2018-06-27 21:38:54 +0900

    Initial Commit

---
+++ MemoPad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoPad
+//
+//  Created by まゆり吉積 on 2018/06/27.
+//  Copyright © 2018年 まゆり吉積. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
